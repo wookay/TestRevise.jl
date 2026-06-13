f81fef186c4fcf95ae4ee6d14b7de2eae8a75afd
WooKyoung Noh <wookay.noh@gmail.com>
2026-06-14 05:43:26 +0900

    test revise_structs

--- test/revise/revise_structs.jl
+++ test/revise/revise_structs.jl
@@ -1,12 +1,15 @@
-using Revise
+module test_revise_structs
 
 using Test
+using Revise
+
 using TestRevise: TestRevise, FAFO
+
+@test FAFO(3) isa FAFO
 @test_throws MethodError FAFO(1, 2)
 
 filepath = normpath(pathof(TestRevise), "../types.jl")
 open(filepath, "w") do f
-    # Make sure we write 64bit integer in little-endian byte order
     write(f, """
 # module TestRevise
 
@@ -19,9 +22,31 @@ end
 """)
 end
 
-const mtimedelay = 3.1
+const mtimedelay = 0.1
 sleep(mtimedelay)
 
 Revise.revise()
 
+@test_throws MethodError FAFO(3)
 @test FAFO(1, 2) isa FAFO
+
+open(filepath, "w") do f
+    write(f, """
+# module TestRevise
+
+struct FAFO
+    field1
+end
+
+# module TestRevise
+""")
+end
+
+sleep(mtimedelay)
+
+Revise.revise()
+
+@test FAFO(3) isa FAFO
+@test_throws MethodError FAFO(1, 2)
+
+end # module test_revise_structs
---
+++ test/revise/visit.jl
@@ -0,0 +1,20 @@
+module test_revise_visit
+
+using Test
+using Revise
+
+Revise.is_typeeq
+Revise.typeeq_parameter
+
+typlike = Type{Vector}
+
+if VERSION >= v"1.14.0-DEV.2291" # julia commit 26145852c4
+Base.isType
+Base.type_parameter
+
+@test Revise.is_typeeq(typlike)
+else
+@test Revise.is_typeeq(typlike) === false
+end # if
+
+end # module test_revise_visit
